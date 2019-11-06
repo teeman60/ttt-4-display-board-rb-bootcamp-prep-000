7ca56bf5a5f7ec069b6e037e0e627e8a21e3b9ff
Taiye Salami <taiye41@gmail.com>
2019-11-06 15:52:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 board[5] = " X "
 end
